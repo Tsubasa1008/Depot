085bbd240199f0519feadfd0913261a0877b79b3
PoWeiLee <tsubasa1008@gmail.com>
2017-06-05 20:28:57 +0800

    Check Out Ver1

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :orders
   resources :line_items do
     member do
       post 'decrease'
@@ -9,6 +10,10 @@ Rails.application.routes.draw do
   resources :carts
   root 'store#index', as: 'store_index'
 
-  resources :products
+  resources :products do
+    member do
+      get 'who_bought'
+    end
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
