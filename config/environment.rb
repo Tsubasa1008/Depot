1f912f94e5896dd22fc3e90a59d0ed666fc8af32
PoWeiLee <tsubasa1008@gmail.com>
2017-06-06 21:32:48 +0800

    Sending Mail Ver2

--- config/environment.rb
+++ config/environment.rb
@@ -4,15 +4,13 @@ require_relative 'application'
 # Initialize the Rails application.
 Rails.application.initialize!
 Depot::Application.configure do
-  config.action_mailer.delivery_method = :smtp
-
   config.action_mailer.smtp_settings = {
     address:        "smtp.gmail.com",
     port:           587,
-    domain:         "test.net",
+    domain:         "gmail.com",
     authentication: "plain",
-    user_name:      "tsubasa",
-    password:       "secret",
+    user_name:      "tanakanaoki1008@gmail.com",
+    password:       "426897513",
     enable_starttls_auto: true
   }
 end
